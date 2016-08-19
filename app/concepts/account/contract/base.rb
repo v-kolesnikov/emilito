2a810cf6ddcc830dd843c5132af396c08cadb0c4
V.Kolesnikov <re.vkolesnikov@gmail.com>
2016-08-19 22:47:05 +0300

    Update Account::Create operation contract
     * Add gem dry-types
     * Update Account concept

--- app/concepts/account/contract/base.rb
+++ app/concepts/account/contract/base.rb
@@ -1,8 +1,8 @@
 class Account
   module Contract
     class Base < Reform::Form
-      property :email, type: String
-      property :password, type: String
+      property :email
+      property :password
     end
   end
 end
--- app/concepts/account/operation/create.rb
+++ app/concepts/account/operation/create.rb
@@ -1,10 +1,19 @@
+require 'reform/form/dry'
+
 class Account
   class Create < Trailblazer::Operation
     include Model
 
     model Account, :create
 
-    contract Account::Contract::Base
+    contract Account::Contract::Base do
+      feature Reform::Form::Dry
+
+      validation do
+        required(:email).filled(:str?)
+        required(:password).filled(:str?)
+      end
+    end
 
     def process(params)
       validate(params) do
