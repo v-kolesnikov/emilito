52b0b5c9fa53c52af63552f2a742e8947f401e6e
V.Kolesnikov <re.vkolesnikov@gmail.com>
2016-08-28 21:52:28 +0300

    Update Web Application layout
     * Refactoring application layout
     * Add Web::StaticPagesController and static pages views

---
+++ app/controllers/web/static_pages_controller.rb
@@ -0,0 +1,35 @@
+module Web
+  class StaticPagesController < Web::ApplicationController
+    # GET /
+    def index
+    end
+
+    # GET /personal
+    def personal
+    end
+
+    # GET /open_source
+    def open_source
+    end
+
+    # GET /business
+    def business
+    end
+
+    # GET /explore
+    def explore
+    end
+
+    # GET /pricing
+    def pricing
+    end
+
+    # GET /blog
+    def blog
+    end
+
+    # GET /support
+    def support
+    end
+  end
+end
