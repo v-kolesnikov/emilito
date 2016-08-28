aec9d48cf9004e35aaa5d65d3949af57fd64441d
V.Kolesnikov <re.vkolesnikov@gmail.com>
2016-08-28 20:15:36 +0300

    Fix Web::Accounts::ApplicationController
     * Remove duplicate method authorize!

--- app/controllers/web/accounts/application_controller.rb
+++ app/controllers/web/accounts/application_controller.rb
@@ -2,10 +2,6 @@ module Web
   module Accounts
     class ApplicationController < Web::ApplicationController
       before_action :authorize!
-
-      def authorize!
-        redirect_to sign_in_path unless logged_in?
-      end
     end
   end
 end
