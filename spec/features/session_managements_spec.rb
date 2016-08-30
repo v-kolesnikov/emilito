74685660d1029ea740b687d28e4bbf86ae5e89ec
V.Kolesnikov <re.vkolesnikov@gmail.com>
2016-08-30 13:46:27 +0300

    Refactoring features spec

--- spec/features/session_managements_spec.rb
+++ spec/features/session_managements_spec.rb
@@ -3,7 +3,7 @@ require 'support/user_context'
 require 'support/shared_contexts/user_signed_in'
 require 'support/shared_examples/user_not_signed_in'
 
-RSpec.feature 'SessionManagements', type: :feature do
+RSpec.feature 'Feature::SessionManagement', type: :feature do
   context 'when user not signed in' do
     scenario '/dashboard redirect to login page' do
       visit '/dashboard'
@@ -12,17 +12,17 @@ RSpec.feature 'SessionManagements', type: :feature do
   end
 
   context 'when registered user sign in' do
-    let(:email) { 'foo@bar.com' }
-    let(:password) { '12345678' }
+    include_context 'user_context' do
+      let(:email) { 'foo@bar.com' }
+      let(:password) { '12345678' }
+    end
 
-    include_context 'user_context'
-
-    scenario 'redirect to /dashboard after sign in' do
+    scenario 'redirect to /dashboard after login' do
       visit '/sign_in'
 
       within('form#new_session') do
-        fill_in :session_email, with: email
-        fill_in :session_password, with: password
+        fill_in :session_email, with: 'foo@bar.com'
+        fill_in :session_password, with: '12345678'
         click_on I18n.t(:sign_in)
       end
 
