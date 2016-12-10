a8d2f8ce714834b53202cf6dcea34771534d9316
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2016-12-10 13:58:34 +0300

    Add comments about routes to controllers

--- app/controllers/web/sessions_controller.rb
+++ app/controllers/web/sessions_controller.rb
@@ -2,10 +2,12 @@ module Web
   class SessionsController < Web::ApplicationController
     before_action :logged_in_behavior!, only: [:new, :create]
 
+    # GET /sign_in
     def new
       form Session::SignIn
     end
 
+    # POST /sign_in
     def create
       run Session::SignIn do |op|
         login(op.model)
@@ -15,6 +17,7 @@ module Web
       render :new, status: :unauthorized
     end
 
+    # DELETE /sign_out
     def destroy
       logout
       redirect_to :root
