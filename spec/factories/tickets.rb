87154324bc999f4dd36363614806dd666e5d77a5
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2016-12-30 10:54:06 +0300

    Refactoring tests factories
    Now we can use default FactoryGirl interface as `create(:factory)` and
    `create_list(:factory, count)` for create concepts through operations.

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
   factory :ticket do
+    skip_create
+
+    workspace
+
     title { Faker::Hipster.sentence }
+
+    initialize_with do
+      Ticket::Create.(ticket: attributes, workspace_id: workspace.id)['model']
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,17 @@
 FactoryGirl.define do
   factory :user do
+    skip_create
+
     email { Faker::Internet.email }
+    login { Faker::Internet.user_name }
     password { Faker::Internet.password }
 
     trait :create_form do
       password_confirmation { password }
+    end
+
+    initialize_with do
+      User::SignUp.(user: attributes)['model']
     end
   end
 end
